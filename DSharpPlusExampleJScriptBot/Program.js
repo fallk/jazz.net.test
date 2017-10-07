e257202528dc42b68841b318b04adda05ec6eee4
chrishansen69 <cadrekucra@gmx.com>
2017-10-07 20:47:16 -0300

    Add project files.

---
+++ DSharpPlusExampleJScriptBot/Program.js
@@ -0,0 +1,14 @@
+import System;
+
+package DSharpPlusExampleJScriptBot{
+    class Program{
+        static function Main(args: String[])
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
+
+DSharpPlusExampleJScriptBot.Program.Main(Environment.GetCommandLineArgs());
